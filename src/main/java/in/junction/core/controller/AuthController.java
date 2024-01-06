f109376311e6131cac14adb28444944bc2b4dc3a
Hardik Singhal <hardik.singhal@wynk.in>
2024-01-06 16:43:06 +0530

    returning map in response

--- src/main/java/in/junction/core/controller/AuthController.java
+++ src/main/java/in/junction/core/controller/AuthController.java
@@ -5,12 +5,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     @GetMapping("/login")
-    public String login(@RequestParam String email) {
-        return email;
+    public Object login(@RequestParam String email) {
+        Map<String, String> response = new HashMap<>();
+        response.put("username", email);
+        return response;
     }
 }
