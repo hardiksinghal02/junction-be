d9a04ad35d3abab73cba96b0ddd4ec78c811d1a7
Hardik Singhal <hardik.singhal@wynk.in>
2024-01-06 14:16:04 +0530

    Initialized application

---
+++ src/main/java/in/junction/core/JunctionCoreApplication.java
@@ -0,0 +1,13 @@
+package in.junction.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunctionCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunctionCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/junction/core/JunctionCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package in.junction.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunctionCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
